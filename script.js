67dc48f92d0e84715824fc1881238f21ffe68d14
Weply <146603597+WeplyStudio@users.noreply.github.com>
2024-05-26 10:35:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/664815b0981b6c564771fe97/1hu4p9ep4';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
+window.addEventListener('devicemotion', function(event) {
+  var acceleration = event.accelerationIncludingGravity;
+  var threshold = 30; // Sesuaikan threshold dengan kebutuhan Anda
+
+  var totalAcceleration = Math.sqrt(
+    Math.pow(acceleration.x, 2) +
+    Math.pow(acceleration.y, 2) +
+    Math.pow(acceleration.z, 2)
+  );
+
+  if (totalAcceleration > threshold) {
+    swal({
+      title: "Perangkat digoyangkan dengan kencang!",
+      icon: "warning",
+      text: "Apakah website mengalami error? jika ada masalah terkait perangkat anda silahkan hubungi kami via email support@weply.my.id. \n\nJangan khawatir! ini hanya peringatan system apabila perangkat anda mengalami error yang mungkin disebabkan oleh website.",
+      buttons: true
+    });
+  }
+});
+
+function showAlert() {
+        alert("Gratis domain .my.id / .biz.id tanpa minimal durasi, Gratis domain senilai 160.000 untuk pembelian minimal 12 bulan")
+        };
+        
+function visitBasic() {
+        window.location.href = "//wa.me/6283898777054?text=Hai%20weply%20studio!%0ASaya%20ingin%20membeli%20web%20hosting%20paket%20basic"
+        };
+
+function visitStandart() {
+        window.location.href = "//wa.me/6283898777054?text=Hai%20weply%20studio!%0ASaya%20ingin%20membeli%20web%20hosting%20paket%20standart"
+        };
+        
+function visitSuper() {
+        window.location.href = "//wa.me/6283898777054?text=Hai%20weply%20studio!%0ASaya%20ingin%20membeli%20web%20hosting%20paket%20super"
+        };
